10f541bd12f09fbbcf27828974e160ca8ac79c02
Tejas-thawari-05 <tejasthawri@gmail.com>
2025-08-11 11:43:44 +0530

    Day-7 module calling form local

---
+++ Day-7-module-source-temp/variables.tf
@@ -0,0 +1,17 @@
+variable "ami" {
+  description = "ami id"
+  type = string
+  default = ""
+}
+
+variable "instance_type" {
+  description = "instance type"
+  type = string
+  default = ""
+}
+
+variable "instance-name" {
+  description = "instance name"
+  type = string
+  default = ""
+}
